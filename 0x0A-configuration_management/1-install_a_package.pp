65a3279c0758db995fbf38e0082893cc0625a7bf
AWWAL3421 <makavellimuhammad@gmail.com>
2024-06-28 07:11:08 -0700

    1-install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-#  install flask from pip3
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/bin',
-  unless  => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
+#!/usr/bin/pup
+# Install a especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
